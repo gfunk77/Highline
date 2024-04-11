355676e3c94d1bb69ab78592732bda7a8ee6c125
gfunk77 <faraci77@gmail.com>
2024-04-11 13:48:32 -0400

    markup

--- highline/src/app/app.routes.ts
+++ highline/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SearchComponent } from './search/search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: SearchComponent,
+    title: 'Search',
+  },
+];
