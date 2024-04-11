b146fdf0dc542acf5247dd5a30ed828aea0f23d9
gfunk77 <faraci77@gmail.com>
2024-04-11 13:53:22 -0400

    add http and service

--- highline/src/app/search/search.component.ts
+++ highline/src/app/search/search.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { SearchService } from './search.service';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.scss'
+  styleUrl: './search.component.scss',
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  searchService: SearchService = inject(SearchService);
 
+  ngOnInit(): void {}
 }
